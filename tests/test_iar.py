8d1e60d34369d86d5001d30d35d37bd2b61a6a1c
Felipe Elorrieta <41742849+felipeelorrieta@users.noreply.github.com>
2022-12-28 19:01:37 -0300

    Newest version of the iAR package

--- tests/test_iar.py
+++ tests/test_iar.py
@@ -1,8 +1,8 @@
-from iar import IAR_sample,IAR_phi_loglik,IAR_loglik,gentime
+from iar import IARsample,IARphiloglik,IARloglik,gentime
 import numpy as np
 np.random.seed(6713)
 sT=gentime(n=100)
-y,sT =IAR_sample(0.99,100,sT)
+y,sT =IARsample(0.99,100,sT)
 
-phi=IAR_loglik(y,sT,0)
+phi=IARloglik(y,sT,0)
 phi
